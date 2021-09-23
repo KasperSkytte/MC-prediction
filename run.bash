2564f6cf9f49afbf3e4fe9c6d68306a3880c919e
Kasper Skytte Andersen <knaldhat@gmail.com>
2021-09-23 16:19:53 +0200

    write to logfile

--- run.bash
+++ run.bash
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
 set -eu
-rm -rf results figures
-mkdir -p results figures data/preprocessed
 
-Rscript preprocess.R
-python main.py
+#set timezone
+export TZ="Europe/Copenhagen"
+
+rm -rf results
+mkdir -p results/figures data/preprocessed
+
+timestamp=$(date '+%Y%m%d_%H%M%S')
+logFile="log_${timestamp}.txt"
+
+main() {
+  echo "#################################################"
+  echo "Script: $(realpath "$0")"
+  echo "System time: $(date '+%Y-%m-%d %H:%M:%S')"
+  echo "Current user name: $(whoami)"
+  echo "Running in docker container:" $(if [ -f '/.dockerenv' ]; then echo yes; else echo no; fi)
+  echo "Current working directory: $(pwd)"
+  echo "Log file: $(realpath -m "$logFile")"
+  echo "Configuration:"
+  cat config.json
+  echo "#################################################"
+  echo
+  Rscript preprocess.R
+  python main.py
+}
+
+main |& tee "$logFile"
+
+mv results "results_${timestamp}"
