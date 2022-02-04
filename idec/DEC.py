51042012eb17e1fd0193a21e9271d08c26398372
Kasper Skytte Andersen <knaldhat@gmail.com>
2022-02-04 20:24:24 +0000

    some keras functions have moved to tensorflow in latest version

--- idec/DEC.py
+++ idec/DEC.py
@@ -21,10 +21,10 @@ Author:
 from time import time
 import numpy as np
 import keras.backend as K
-from keras.engine.topology import Layer, InputSpec
+from tensorflow.keras.layers import Layer, InputSpec
 from keras.layers import Dense, Input
 from keras.models import Model
-from keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD
 from keras.utils.vis_utils import plot_model
 
 from sklearn.cluster import KMeans
